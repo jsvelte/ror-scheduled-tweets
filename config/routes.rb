7f775ac9d5c396dc43add24807c59b4bd8d2e056
jsvelte <joshua.galit@sun-asterisk.com>
2023-03-29 12:56:09 +0800

    [ST-831] - [FE/BE] Implement Login Functionality

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,14 @@
 Rails.application.routes.draw do
   get "about", to: "about#index"
 
+  # Sign Up
   get "sign_up", to: "registrations#new"
   post "sign_up", to: "registrations#create"
+
+  # Log In
+  get "sign_in", to: "sessions#new"
+  post "sign_in", to: "sessions#create"
+
   delete "logout", to: "sessions#destroy"
 
   root to: "main#index"
