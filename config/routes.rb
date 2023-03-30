531cc471517b6a18c3acd8d9847d2e0a0c99f765
jsvelte <joshua.galit@sun-asterisk.com>
2023-03-30 10:28:40 +0800

    [ST-876] - [FE/BE] Implement Edit Password

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,10 @@ Rails.application.routes.draw do
   get "sign_in", to: "sessions#new"
   post "sign_in", to: "sessions#create"
 
+  # Edit Password
+  get "password", to: "passwords#edit", as: :edit_password
+  patch "password", to: "passwords#update"
+
   delete "logout", to: "sessions#destroy"
 
   root to: "main#index"
